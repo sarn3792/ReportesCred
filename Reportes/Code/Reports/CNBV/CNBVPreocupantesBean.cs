8a2ec5101b8adaeb31a3a866ab281dc685e634eb
sarn3792 <sarn3792@gmail.com>
2017-07-20 13:49:34 -0500

    ; was added at the end of CNBV reports.

--- Reportes/Code/Reports/CNBV/CNBVPreocupantesBean.cs
+++ Reportes/Code/Reports/CNBV/CNBVPreocupantesBean.cs
@@ -640,7 +640,7 @@ namespace Reportes
             String allData = String.Empty;
             try
             {
-                allData = String.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10:0.00};{11};{12};{13};{14};{15};{16};{17};{18};{19};{20};{21};{22};{23};{24};{25};{26};{27};{28};{29};{30};{31};{32};{33};{34};{35}",
+                allData = String.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10:0.00};{11};{12};{13};{14};{15};{16};{17};{18};{19};{20};{21};{22};{23};{24};{25};{26};{27};{28};{29};{30};{31};{32};{33};{34};{35};",
                                       TipoReporte, PeriodoReporte, Folio, OrganoSupervisor, ClaveSujetoObligado, Localidad, CodigoPostal, TipoOperacion, InstrumentoMonetario, NumeroCuenta, Monto, Moneda, FechaOperacion,
                                       FechaDeteccion, Nacionalidad, TipoPersona, RazonSocial, Nombre, ApellidoPaterno, ApellidoMaterno, RFC, CURP, FechaNacimiento, Domicilio, Colonia, Ciudad, Telefono, ActividadEconomica,
                                       ConsecutivoCuentas, NumeroCuentaC, ClaveSujetoObligadoC, NombreTitularCuenta, ApellidoPaternoC, ApellidoMaternoC, DescripcionOperacion, Razones);
